59f968fb4a7b94ea43569ca8caa1bd897de5f172
alackfeng <fengyue7576199@163.com>
2022-07-18 17:50:35 +0800

    add centos systemctl service

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-nohup ./bin/bytezero server --config ./scripts/.bytezero.idc6.yaml > bzserver.log&
+## nohup ./bin/bytezero server --config ./scripts/.bytezero.idc6.yaml > bzserver.log&
+/home/vagrant/bytezero-go/bin/bytezero server --config ./scripts/.bytezero.116.yaml --daemon
