126325bd5adeca6b670f90361f8ef06ee1d5f7ac
alackfeng <fengyue7576199@163.com>
2023-03-29 18:04:58 +0800

    update sysstate Get speed linux ok

--- apps/sysstat/sysnet_linux.go
+++ apps/sysstat/sysnet_linux.go
@@ -2,7 +2,12 @@ package sysstat
 
 import (
 	"net"
-	"syscall"
+	"strings"
+	"fmt"
+	"strconv"
+	"regexp"
+	"os/exec"
+
 )
 
 // GetNetworkMaxSpeed - 获取网卡最大速率.
@@ -13,10 +18,11 @@ func GetNetworkMaxSpeed(networkInterface net.Interface) (uint32, error) {
 		return 0, err
 	}
 	reg := regexp.MustCompile("Speed:(.*)Mb/s")
-	res := reg.FindStringSubmatch(test)
+	res := reg.FindStringSubmatch(string(output))
+	fmt.Println("GetNetworkMaxSpeed:", networkInterface.Name, res)
 	if len(res) != 2 {
-		return 0, fmt.Errorf("regexp speed error")
+		return 0, nil
 	}
 	speed, _ := strconv.ParseInt(strings.Trim(res[1], " "), 10, 64)
-	return speed, nil
+	return uint32(speed), nil
 }
