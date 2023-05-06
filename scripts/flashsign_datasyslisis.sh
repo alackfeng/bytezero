b1f3cb1dc7800634531afe0da9360c1dc034cad2
linxl <1658370535@qq.com>
2023-05-06 13:56:11 +0800

    add flashsign month script

--- scripts/flashsign_datasyslisis.sh
+++ scripts/flashsign_datasyslisis.sh
@@ -2,7 +2,7 @@
 
 ## 每天记录.
 #### /opt/bytezero/bin/bytezero flashsign --last-report-date "2022-12-05 00:00:00"
-## 月度记录.
-#### /opt/bytezero/bin/bytezero flashsign --table-field revenueMonth --last-report-date "2022-12-01 00:00:00" 
+## 4月度记录.
+#### /opt/bytezero/bin/bytezero flashsign --table-field revenueMonth --last-report-date "2023-04-01 00:00:00"
 
 nohup /opt/bytezero/bin/bytezero flashsign >> /opt/bytezero/bz.log &
